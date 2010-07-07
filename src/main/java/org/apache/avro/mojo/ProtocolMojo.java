f475e883e5398d92f39c1ed31c4c86288c2cc65d
spullara <sam@sampullara.com>
2010-07-07 15:57:53 -0700

    upgrade to the protocol compiling avrocompiler

--- src/main/java/org/apache/avro/mojo/ProtocolMojo.java
+++ src/main/java/org/apache/avro/mojo/ProtocolMojo.java
@@ -88,6 +88,11 @@ public class ProtocolMojo extends AbstractMojo {
      */
     private String schemaExtension;
 
+    /**
+     * @parameter default-value=".avrodl"
+     */
+    private String avrodlExtension;
+
     private FileSetManager fileSetManager = new FileSetManager();
 
     public void execute() throws MojoExecutionException {
@@ -121,6 +126,8 @@ public class ProtocolMojo extends AbstractMojo {
                             templateSubdir,
                             new File(sourceDirectory, filename),
                             outputDirectory);
+                } else if (filename.endsWith(avrodlExtension)) {
+                    
                 } else {
                     throw new MojoExecutionException("Do not know whether " + filename + " is a protocol or a schema");
                 }
